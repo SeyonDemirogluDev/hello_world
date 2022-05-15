a2e8af75a554a3e30376b8164aad2406b4b96479
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-05-15 17:31:08 +0200

    init

---
+++ HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier
+
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+
+    function helloWorld() public pure returns (string memory) {
+        string memory greeting = "Hello world!";
+        return greeting;
+    }
+}
