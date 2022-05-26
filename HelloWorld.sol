adbbb8cc72197c07be00fff2ab539b0dcfea573c
Seyon Demiroglu <seyon.demiroglu01@gmail.com>
2022-05-26 22:48:15 +0200

    Hello, world!

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 contract HelloWorld {
 
     function helloWorld() public pure returns (string memory) {
-        string memory greeting = "Hello world!";
+        string memory greeting = "Hello, world!";
         return greeting;
     }
 }
